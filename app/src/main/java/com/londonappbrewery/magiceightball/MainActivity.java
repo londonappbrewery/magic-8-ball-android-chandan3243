e9dffcf7b52ac24ed13453e85f770cec2bfad90d
Chandan <chandan.singh.7574@gmail.com>
2018-04-19 15:53:36 -0700

    Completed

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,15 @@
 package com.londonappbrewery.magiceightball;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+      final   Button bt = (Button) findViewById(R.id.button);
+       final ImageView main = (ImageView) findViewById(R.id.imageView);
+        final TextView textView = (TextView) findViewById(R.id.textView);
+
+       final int[] Ballarray = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random RNgenerator = new Random();
+                int pi = RNgenerator.nextInt(5);
+                main.setImageResource(Ballarray[pi]);
+            }
+        });
+
     }
 }
